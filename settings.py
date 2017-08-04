1d5021d2d4f7cac8abe241ef5ac1c69e65ade176
Alex Casalboni <alexsaxmib@gmail.com>
2017-08-04 17:11:10 +0200

    Moved files to new folders, refactored training and flask code, pylinted around

--- settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-training_data_filename = os.path.join(dir_path, u"data", u"creditcard.csv")
-model_dump_filename = os.path.join(dir_path, u"models", u"fraud_detection.pickle")
--- src/flask_app.py
+++ src/flask_app.py
@@ -1,25 +1,23 @@
-from flask import Flask
-from flask import request
+from __future__ import unicode_literals
 import json
-
+from flask import Flask, request
 from sklearn.externals import joblib
+from settings import MODEL_FILENAME
 
-from settings import model_dump_filename
+app = Flask("Fraud Detection")
 
-app = Flask(__name__)
+# load model at startup time
+app.model = joblib.load(MODEL_FILENAME)
 
-engine = joblib.load(model_dump_filename)
-
-
-@app.route(u"/fraud_score", methods=[u"POST"])
-def fraud_score():
+@app.route(u"/predict", methods=[u"POST"])
+def predict_fraud():
     input_data = request.get_json()
     if u"features" not in input_data:
         return json.dumps({u"error": u"No features found in input"}), 400
     if not input_data[u"features"] or not isinstance(input_data[u"features"], list):
         return json.dumps({u"error": u"No feature values available"}), 400
     if isinstance(input_data[u"features"][0], list):
-        results = engine.predict_proba(input_data[u"features"]).tolist()
+        results = app.model.predict_proba(input_data[u"features"]).tolist()
     else:
-        results = engine.predict_proba([input_data[u"features"]]).tolist()
+        results = app.model.predict_proba([input_data[u"features"]]).tolist()
     return json.dumps({u"scores": [result[1] for result in results]}), 200
